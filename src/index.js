45c277e6586b722f6b38c66e2ebfba30b36cea0f
Art <art_jr2000@yahoo.com>
2017-09-13 16:55:40 -0500

    added body string

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 /**
  * es6 modules and imports
  */
+
+import $ from "jquery";
 import sayHello from './hello';
 sayHello('World');
 
@@ -10,10 +12,14 @@ sayHello('World');
 const getMovies = require('./getMovies.js');
 
 getMovies().then((movies) => {
-  console.log('Here are all the movies:');
+  let htmlStr = "<div>";
+
   movies.forEach(({title, rating, id}) => {
-    console.log(`id#${id} - ${title} - rating: ${rating}`);
+    htmlStr += `<h3>id#${id} - ${title} - rating: ${rating}</h3>`;
   });
+  htmlStr += "</div>";
+  $("body").html(htmlStr);
+
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.')
   console.log(error);
